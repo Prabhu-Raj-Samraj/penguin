111d26af220e2d4fe2f66a2129a27203f6d87370
Prabhu Raj Samraj <prabhuraj.sp@gmail.com>
2025-07-30 08:32:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
  
 st.title("Penguine Species Prediction ML App")
+st.info("This ios end to end Machine Learning App")
+
+with st.expander("Data"):
+ pass
