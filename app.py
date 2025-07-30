286ae246934bf7583504d02648a7f7ae59f831a7
Prabhu Raj Samraj <prabhuraj.sp@gmail.com>
2025-07-30 09:35:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -41,9 +41,6 @@ with st.expander("Data Visualization"):
   sns.scatterplot(data=df, x='bill_length_mm', y='body_mass_g', hue='species', ax=ax)
   st.pyplot(fig)
  
-with st.expander("Input data"):
-  pass
- 
 with st.expander("Data Preperation"):
   pass
  
@@ -55,3 +52,24 @@ with st.sidebar:
   flipper_length_mm = st.slider('Flipper length (mm)',172.0,231.0,201.0)
   body_mass_g = st.slider('Body mass (g)',2700.0,6300.0,4207.0)
   gender = st.selectbox('Gender',('male','female'))
+
+  data = {
+    'island': 'Island where the penguin was observed (Biscoe, Dream, Torgersen)',
+    'bill_length_mm': 'Bill length of the penguin in millimeters (mm)',
+    'bill_depth_mm': 'Bill depth (height) of the penguin in millimeters (mm)',
+    'flipper_length_mm': 'Flipper length of the penguin in millimeters (mm)',
+    'body_mass_g': 'Body mass of the penguin in grams (g)',
+    'gender': 'Biological sex of the penguin (Male or Female)'
+}
+input_df = pd.DataFrame(data, index = [0])
+input_penguins = pd.concat([input_df, X_raw], axis = 0)
+
+with st.expander("Input data"):
+  st.write("**Input data**")
+  input_df
+  st.write("**Combineed data**")
+  input_penguins
+  
+
+
+
