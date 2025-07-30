11d8f5d1430a51ce565f69d1a65629a5d270cbee
Prabhu Raj Samraj <prabhuraj.sp@gmail.com>
2025-07-30 08:57:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,26 @@ import streamlit as st
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
  
-st.title("Penguine Species Prediction ML App")
-st.info("This ios end to end Machine Learning App")
-
+st.title("Penguine Speicy Prediction ML app")
+st.info("This is end-to-end Machine Learning App")
+ 
 with st.expander("Data"):
- pass
-
+  pass
+ 
 with st.expander("Data Visualization"):
- pass
-
-with st.expander("Input Data"):
- pass
-
-with st.expander("Data Preparation"):
- pass
-
+  pass
+ 
+with st.expander("Input data"):
+  pass
+ 
+with st.expander("Data Preperation"):
+  pass
+ 
 with st.sidebar:
- st.header("Input Variables")
+  st.header("Input Variables")
+  island = st.selectbox('Island',('Biscoe','Dream','Torgersen'))
+  bill_length_mm = st.slider('Bill length (mm)',32.1,59.6,43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)',13.1,21.5,17.2)
+  flipper_length_mm = st.slider('Flipper length (mm)',172.0,231.0,201.0)
+  body_mass_g = st.slider('Body mass (g)',2700.0,6300.0,4207.0)
+  gender = st.selectbox('Gender',('male','female'))
