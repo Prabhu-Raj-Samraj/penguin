c11d0d6ac020135e2c4344f0d4edf3e347165638
Prabhu Raj Samraj <prabhuraj.sp@gmail.com>
2025-07-30 08:36:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,12 @@ with st.expander("Data"):
 
 with st.expander("Data Visualization"):
  pass
+
+with st.expander("Input Data"):
+ pass
+
+with st.expander("Data Preparation"):
+ pass
+
+with st.sidebar:
+ pass
